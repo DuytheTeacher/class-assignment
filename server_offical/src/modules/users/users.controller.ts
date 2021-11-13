b7b464cc0690cf8a2149747bf9c55ed3ba42e699
datle <letyendat@gmail.com>
2021-11-13 08:43:02 +0700

    BodyResponse

--- server_offical/src/modules/users/users.controller.ts
+++ server_offical/src/modules/users/users.controller.ts
@@ -2,18 +2,20 @@ import { TokenData } from "@modules/auth";
 import { NextFunction, Request, Response } from "express";
 import RegisterDto from "./dtos/register.dto";
 import UserService from "./users.service";
+import BodyRespone from "@core/response_default";
 
 export default class UserController {
-    private userService = new UserService();
+  private userService = new UserService();
 
-    public register = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const model: RegisterDto = req.body;
-           const tokenData: TokenData = await this.userService.createUser(model);
+  public register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const model: RegisterDto = req.body;
+      const tokenData: TokenData = await this.userService.createUser(model);
 
-           res.status(201).json(tokenData);
-        } catch (error) {
-            next(error);
-        }
+      const resp = new BodyRespone("Success", tokenData);
+      res.status(201).json(resp);
+    } catch (error) {
+      next(error);
     }
+  };
 }
